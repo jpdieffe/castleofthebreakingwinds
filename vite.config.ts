089ea6ed375ae2c109368f8e9137b8629a6a1d0d
Jimmy <jpdieffe@ncsu.edu>
2026-03-15 21:25:26 -0400

    Build to docs/, deploy from main branch

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // Set this to your GitHub repo name for GitHub Pages deployment.
-  // e.g. if your repo is github.com/yourname/CastleOfTheBreakingWinds
-  // set base to "/CastleOfTheBreakingWinds/"
   base: "/castleofthebreakingwinds/",
+  build: {
+    outDir: "docs",
+  },
 });
